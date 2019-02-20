debacd7b8f666b5004e4aad6144a2e6c583d60be
smyjpmu <38014984+smyjpmu@users.noreply.github.com>
2019-02-20 04:43:59 -0500

    Sway, btrfs or ext4, bugs

--- iabacli.sh
+++ iabacli.sh
@@ -21,6 +21,9 @@
 	LSBLK="$(lsblk)"
 	TARGET_DRIVE=$(whiptail --inputbox "/dev/sd* (replace the * with the drive letter no number) $LSBLK" 15 60 /dev/ --title "target drive" 3>&1 1>&2 2>&3)
 
+# File System
+	FILESYSTEM=$(whiptail --menu "select=[ENTER]" 12 50 5 --title "file systems" 3>&1 1>&2 2>&3 "ext4" "Recommended and is nearly indestructible." "btrfs" "Use only if you need certain features.")
+
 # Bootloader ID
 	BOOTLOADER_ID=$(whiptail --inputbox "done=[ENTER], write the bootloader ID." 8 60 Arch --title "bootloader id" 3>&1 1>&2 2>&3)
 
@@ -30,37 +33,60 @@
 # Username
 	USERNAME=$(whiptail --inputbox "done=[ENTER], write your username." 8 60 noname --title "username" 3>&1 1>&2 2>&3)
 
-# Password
-	PASSWORD=$(whiptail --passwordbox "done=[ENTER], enter the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-	PASSWORD2=$(whiptail --passwordbox "done=[ENTER], re-enter the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-	if [ "$PASSWORD" != "$PASSWORD2" ]; then
+# User Password
+	PWD=$(whiptail --passwordbox "done=[ENTER], enter the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+	PWD2=$(whiptail --passwordbox "done=[ENTER], re-enter the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+	if [ "$PWD" != "$PWD2" ]; then
 		whiptail --msgbox "Passowrds did not match, please try again. (2 tries left)" --title "failed password" 8 60
-		PASSWORD=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-		PASSWORD2=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-		if [ "$PASSWORD" != "$PASSWORD2" ]; then
+		PWD=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+		PWD2=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+		if [ "$PWD" != "$PWD2" ]; then
 			whiptail --msgbox "Passowrds did not match, please try again. (last try)" --title "failed password" 8 60
-			PASSWORD=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-			PASSWORD2=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
-			if [ "$PASSWORD" != "$PASSWORD2" ]; then
+			PWD=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+			PWD2=$(whiptail --passwordbox "done=[ENTER], write the password for user." 8 60 --title "password for user" 3>&1 1>&2 2>&3)
+			if [ "$PWD" != "$PWD2" ]; then
+				exit
+			fi
+		fi
+	fi
+
+# User Password
+	ROOT_PWD=$(whiptail --passwordbox "done=[ENTER], enter the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+	ROOT_PWD2=$(whiptail --passwordbox "done=[ENTER], re-enter the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+	if [ "$ROOT_PWD" != "$ROOT_PWD2" ]; then
+		whiptail --msgbox "Passowrds did not match, please try again. (2 tries left)" --title "failed password" 8 60
+		ROOT_PWD=$(whiptail --passwordbox "done=[ENTER], write the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+		ROOT_PWD2=$(whiptail --passwordbox "done=[ENTER], write the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+		if [ "$ROOT_PWD" != "$ROOT_PWD2" ]; then
+			whiptail --msgbox "Passowrds did not match, please try again. (last try)" --title "failed password" 8 60
+			ROOT_PWD=$(whiptail --passwordbox "done=[ENTER], write the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+			ROOT_PWD2=$(whiptail --passwordbox "done=[ENTER], write the password for root." 8 60 --title "password for root" 3>&1 1>&2 2>&3)
+			if [ "$ROOT_PWD" != "$ROOT_PWD2" ]; then
 				exit
 			fi
 		fi
 	fi
 
 # Interface
-	INTERFACE=$(whiptail --menu "select=[ENTER]" 18 80 10 --title "interface" 3>&1 1>&2 2>&3 "nodeorwm" "Comes with nothing and is nothing." "budgie" "Modern design, focuses on simplicity and elegance." "cinnamon" "Strives to provide a traditional user experience." "gnome" "An attractive and intuitive desktop." "kde" "Modern and familiar working environment." "lxde" "Strives to be less CPU and RAM intensive." "lxqt" "Lightweight, modular, blazing-fast and user-friendly." "mate" " Intuitive and attractive desktop using traditional metaphors." "xfce" "Traditional UNIX philosophy of modularity and re-usability." "i3wm" "Primarily targeted at developers and advanced users")
+	INTERFACE=$(whiptail --menu "select=[ENTER]" 18 80 10 --title "interface" 3>&1 1>&2 2>&3 "nodeorwm" "Comes with nothing and is nothing." "budgie" "Modern design, focuses on simplicity and elegance." "cinnamon" "Strives to provide a traditional user experience." "gnome" "An attractive and intuitive desktop." "kde" "Modern and familiar working environment." "lxde" "Strives to be less CPU and RAM intensive." "lxqt" "Lightweight, modular, blazing-fast and user-friendly." "mate" " Intuitive and attractive desktop using traditional metaphors." "xfce" "Traditional UNIX philosophy of modularity and re-usability." "i3wm" "Primarily targeted at developers and advanced users" "sway" "Drop-in replacement of i3/i3-gaps for Wayland")
 
 # Display manager
-	DISPLAYMANAGER=$(whiptail --menu "select=[ENTER]" 12 50 5 --title "display manager" 3>&1 1>&2 2>&3 "nodm" "Comes with nothing and is nothing." "gdm" "Recommended for Budgie & Gnome." "lightdm" "Recommended for XFCE." "lxdm" "Recommended for LXDE." "sddm" "Recommended for KDE & LXQT.")
+	DISPLAYMANAGER=$(whiptail --menu "select=[ENTER]" 12 50 5 --title "display manager" 3>&1 1>&2 2>&3 "nodm" "Comes with nothing and is nothing." "gdm" "Recommended for Budgie & Gnome." "lightdm" "Recommended for XFCE." "lxdm" "Recommended for LXDE." "sddm" "Recommended for KDE & LXQT." "ly" "TUI based")
 
 # BlackArch
 	BLACKARCH=$(whiptail --menu "select=[ENTER], default=(false)" 8 60 2 --title "BlackArch" 3>&1 1>&2 2>&3 "false" "Do not install BlackArch tools" "true" "Install BlackArch tools")
 
 # Custom packages
-	CUSTOM_PACKAGES=$(whiptail --separate-output --checklist "select=[space], done=[enter]" 30 50 22 --title "custom packages" 3>&1 1>&2 2>&3 "firefox" "Web Browser" ON "libreoffice" "libreoffice" ON "deluge" "torrent manager" ON "gimp" "image manipulator" ON "audacity" "audio editor" ON "blender" "3d editor" ON "darktable" "photo editor" ON "inkscape" "vector editor" ON "krita" "drawing editor" ON "steam" "steam" ON "playonlinux" "wine manager" ON "lutris" "wine manager" ON)
+	CUSTOM_PACKAGES=$(whiptail --separate-output --checklist "select=[space], done=[enter]" 30 50 22 --title "custom packages" 3>&1 1>&2 2>&3 "firefox" "Web Browser" ON "atom" "IDE" ON "weechat" "IRC client" ON "libreoffice" "Office suite" ON "tor" "proxy" ON "deluge" "torrent manager" ON "gimp" "image manipulator" ON "audacity" "audio editor" ON "blender" "3d editor" ON "darktable" "photo editor" ON "inkscape" "vector editor" ON "krita" "drawing editor" ON "steam" "Game client" OFF "playonlinux" "wine manager" OFF "lutris" "wine manager" OFF)
 
 # Other custom packages
 	OTHER_CUSTOM_PACKAGES=$(whiptail --inputbox "done=[ENTER]" 8 60 --title "other custom packages" 3>&1 1>&2 2>&3)
+
+# Custom packages
+	AUR_CUSTOM_PACKAGES=$(whiptail --separate-output --checklist "select=[space], done=[enter]" 30 50 22 --title "custom packages" 3>&1 1>&2 2>&3 "discord" "discord" ON "spotify" "spotify" OFF "polybar" "polybar" ON)
+
+# Other custom packages
+	AUR_OTHER_CUSTOM_PACKAGES=$(whiptail --inputbox "done=[ENTER]" 8 60 --title "other custom packages" 3>&1 1>&2 2>&3)
 
 # Nvme drive
 	NVME=$(whiptail --menu "select=[ENTER], default=(false)" 8 60 2 --title "nvme" 3>&1 1>&2 2>&3 "false" "I don't have an Nvme SSD." "true" "I have an Nvme SSD.")
@@ -115,7 +141,9 @@
 	elif [ "$INTERFACE" == "xfce" ]; then
 		DE="exo garcon gtk-xfce-engine thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop xfwm4 xfwm4-themes mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfburn xfce4-artwork xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-dict xfce4-diskperf-plugin xfce4-eyes-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mailwatch-plugin xfce4-mount-plugin xfce4-mpc-plugin xfce4-netload-plugin xfce4-notes-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-sensors-plugin xfce4-smartbookmark-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-time-out-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-weather-plugin xfce4-xkb-plugin parole ristretto xfce4-whiskermenu-plugin"
 	elif [ "$INTERFACE" == "i3wm" ]; then
-		DE="i3-gaps rofi rxvt-unicode ranger mc pcurses neofetch cmus calcurse bc"
+		DE="i3-gaps rofi rxvt-unicode ranger mc pcurses neofetch cmus calcurse bc scrot"
+	elif [ "$INTERFACE" == "sway" ]; then
+		DE="sway rofi rxvt-unicode ranger mc pcurses neofetch cmus calcurse bc scrot"
 	elif [ "$INTERFACE" == "nodeorwm" ]; then
 		DE=""
 	fi
@@ -129,6 +157,8 @@
 		DM="lxdm"
 	elif [ "$DISPLAYMANAGER" == "lightdm" ]; then
 		DM="lightdm lightdm-gtk-greeter"
+	elif [ "$DISPLAYMANAGER" == "ly" ]; then
+		AUR_DM="ly-git"
 	elif [ "$DISPLAYMANAGER" == "nodm" ]; then
 		DM=""
 	fi
@@ -152,11 +182,11 @@
 # Packages
 	BASE="bash bzip2 coreutils cryptsetup device-mapper dhcpcd diffutils e2fsprogs file filesystem findutils gawk gcc-libs gettext glibc grep gzip inetutils iproute2 iputils jfsutils less licenses linux logrotate lvm2 man-db man-pages mdadm nano netctl pacman pciutils perl procps-ng psmisc reiserfsprogs s-nail sed shadow sysfsutils systemd-sysvcompat tar texinfo usbutils util-linux vi which xfsprogs"
 	BASE_DEVEL="autoconf automake binutils bison fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman patch pkgconf sed sudo systemd texinfo util-linux which"
-	BLACKARCHTOOLS=""
-	PACKAGES="$BASE $BASE_DEVEL $DE $DM $CUSTOM_PACKAGES $OTHER_CUSTOM_PACKAGES mesa xorg-server networkmanager grub efibootmgr go unzip p7zip unrar curl wget git pulseaudio vlc zsh openssh tor vim openvpn networkmanager-openvpn arandr udiskie"
+	PACKAGES="$BASE $BASE_DEVEL $DE $DM $COMPOSITOR $CUSTOM_PACKAGES $OTHER_CUSTOM_PACKAGES mesa xorg-server networkmanager grub efibootmgr go unzip p7zip unrar curl wget git pulseaudio vlc zsh openssh vim openvpn networkmanager-openvpn arandr udiskie"
+	AUR_PACKAGES="$AUR_CUSTOM_PACKAGES $AUR_OTHER_CUSTOM_PACKAGES"
 
 # unmounting drives
-	echo "-==unmounting drives==-"
+	echo "-==Unmounting Drives==-"
 	if $NVME; then
 		umount ${TARGET_DRIVE}p1 /mnt/boot
 	else
@@ -172,26 +202,26 @@
 	echo "-==Starting Arch Installation==-"
 	timedatectl set-ntp true
 
-	echo "-==checking if system is capeable of EFI==-"
+	echo "-==Checking If System Is Capeable Of EFI==-"
 	if ls /sys/firmware/efi/efivars > /dev/null 2>&1; then
 		EFI=true
 	else
 		EFI=false
 	fi
 
-	echo "-==checking if $TARGET_DRIVE is an SSD==-"
+	echo "-==Checking If $TARGET_DRIVE Is An SSD==-"
 	if [ "$(cat /sys/block/$(echo $TARGET_DRIVE | cut -d'/' -f3)/queue/rotational)" = "0" ]; then
 		SSD=true
-		echo "-==$TARGET_DRIVE is an SSD, trim will be enabled for cryptsetup==-"
+		echo "-==$TARGET_DRIVE Is An SSD, Trim Will Be Enabled For Cryptsetup==-"
 	fi
 
-	echo "-==Formatting drives/partitions==-"
-	sgdisk -og ${TARGET_DRIVE}
+	echo "-==Formatting Drives/Partitions==-"
+	sgdisk -og $TARGET_DRIVE
 	if $EFI; then
-		sgdisk -n 1:0:+512M -c 1:"EFI" -t 1:ef00 ${TARGET_DRIVE}
+		sgdisk -n 1:0:+512M -c 1:"EFI" -t 1:ef00 $TARGET_DRIVE
 	fi
 	SYSTEM_PARTITION=$(if $EFI; then echo 2; else echo 1; fi)
-	sgdisk -n $SYSTEM_PARTITION:0:0 -c $SYSTEM_PARTITION:"System" -t $SYSTEM_PARTITION:8300 ${TARGET_DRIVE}
+	sgdisk -n $SYSTEM_PARTITION:0:0 -c $SYSTEM_PARTITION:"System" -t $SYSTEM_PARTITION:8300 $TARGET_DRIVE
 
 	if $NVME; then
 		if $EFI; then
@@ -200,9 +230,9 @@
 		if $ENCRYPT_DRIVE; then
 			cryptsetup -y -v luksFormat --type luks2 ${TARGET_DRIVE}p$SYSTEM_PARTITION
 			cryptsetup open $(if $SSD; then echo "--allow-discards"; fi) ${TARGET_DRIVE}p$SYSTEM_PARTITION cryptroot
-			mkfs.ext4 /dev/mapper/cryptroot
+			mkfs.$FILESYSTEM /dev/mapper/cryptroot
 		else
-			mkfs.ext4 ${TARGET_DRIVE}p$SYSTEM_PARTITION
+			mkfs.$FILESYSTEM ${TARGET_DRIVE}p$SYSTEM_PARTITION
 		fi
 	else
 		if $EFI; then
@@ -210,23 +240,23 @@
 		fi
 		if $ENCRYPT_DRIVE; then
 			cryptsetup -y -v luksFormat --type luks2 ${TARGET_DRIVE}2
-			cryptsetup open $(if $SSD; then echo "--allow-discards"; fi) ${TARGET_DRIVE}$SYSTEM_PARTITION cryptroot
-			mkfs.ext4 /dev/mapper/cryptroot
+			cryptsetup open $(if $SSD; then echo "--allow-discards"; fi) $TARGET_DRIVE$SYSTEM_PARTITION cryptroot
+			mkfs.$FILESYSTEM /dev/mapper/cryptroot
 		else
-			mkfs.ext4 ${TARGET_DRIVE}$SYSTEM_PARTITION
+			mkfs.$FILESYSTEM $TARGET_DRIVE$SYSTEM_PARTITION
 		fi
 	fi
 
 	sgdisk -p ${TARGET_DRIVE}
 
-	echo "-==Mouting formatted drives==-"
+	echo "-==Mouting Formatted Drives==-"
 	if $ENCRYPT_DRIVE; then
 		mount /dev/mapper/cryptroot /mnt
 	else
 		if $NVME; then
 				mount ${TARGET_DRIVE}p$SYSTEM_PARTITION /mnt/
 		else
-			mount ${TARGET_DRIVE}$SYSTEM_PARTITION /mnt/
+			mount $TARGET_DRIVE$SYSTEM_PARTITION /mnt/
 		fi
 	fi
 	if $EFI; then
@@ -239,11 +269,28 @@
 	fi
 
 # Install packages
-	echo "-==Installing base packages==-"
+	echo "-==Installing Base Packages==-"
 	if [[ "$INTERFACE" == KDE && "$DISPLAYMANAGER" == "$sddm" ]]; then
-  	pacstrap /mnt ${PACKAGES} sddm-kcm
+  	pacstrap /mnt $PACKAGES sddm-kcm
 	else
 		pacstrap /mnt $PACKAGES
+	fi
+
+# Installing yay
+	echo "-==Installing Yay==-"
+	arch-chroot /mnt git clone https://aur.archlinux.org/yay.git
+	arch-chroot /mnt cd yay/
+	arch-chroot /mnt makepkg -si
+	arch-chroot /mnt yay -S $AUR_DM $AUR_PACKAGES
+
+# Installing blackarch
+	if [ "$BLACKARCH" == "true" ]; then
+		echo "-==Adding BlackArch Repository==-"
+		arch-chroot /mnt curl -O https://blackarch.org/strap.sh
+		arch-chroot /mnt chmod +x strap.sh
+		arch-chroot /mnt ./strap.sh
+		echo "-==Installing BlackArch tools==-"
+		arch-chroot /mnt pacman -S blackarch
 	fi
 
 # Encrypt drive
@@ -272,22 +319,24 @@
 # Add user
 	echo "-==Adding Normal User==-"
 	arch-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $USERNAME
-	arch-chroot /mnt echo $USERNAME:$PASSWORD | chpasswd
+	echo "root:$ROOT_PWD" | chpasswd -R /mnt
+	echo "$USERNAME:$PWD" | chpasswd -R /mnt
+	arch-chroot /mnt echo "$USERNAME ALL=(ALL) ALL" > /etc/sudoers
 
 # Set hostname
 	echo "-==Setting Hostname==-"
-	echo "${HOSTNAME}" > /mnt/etc/hostname
-	echo "127.0.0.1 localhost ${HOSTNAME}" >> /mnt/etc/hosts
+	echo "$HOSTNAME" > /mnt/etc/hostname
+	echo "127.0.0.1 localhost $HOSTNAME" >> /mnt/etc/hosts
 
 # Install grub
 	echo "-==Installing GRUB==-"
 	arch-chroot /mnt mkinitcpio -p linux
 	arch-chroot /mnt grub-install --recheck $(if $EFI; then echo "--target=x86_64-efi --efi-directory=/boot --bootloader-id=$BOOTLOADER_ID"; else echo "--target=i386-pc $TARGET_DRIVE"; fi)
-	echo "-==creating GRUB configuration==-"
+	echo "-==Creating GRUB Configuration==-"
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable services
-	echo "-==Enabling services==-"
+	echo "-==Enabling Services==-"
 	arch-chroot /mnt systemctl enable NetworkManager
 	if [ "$DISPLAYMANAGER" != "nodm" ]; then
 		arch-chroot /mnt systemctl enable $DISPLAYMANAGER
@@ -295,16 +344,4 @@
 	arch-chroot /mnt systemctl enable ntpd
 	arch-chroot /mnt systemctl enable tor
 
-# Installing yay
-	echo "-==Installing yay==-"
-	arch-chroot /mnt git clone https://aur.archlinux.org/yay.git && cd yay/ && makepkg -si
-
-# Installing blackarch
-	if [ "$BLACKARCH" == "true" ]; then
-		echo "-==Adding BlackArch repository==-"
-		arch-chroot /mnt curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && ./strap.sh
-		echo "-==Installing BlackArch tools==-"
-		arch-chroot /mnt pacman -S blackarch
-	fi
-
-	echo "-==Arch is ready to be used==-"
+	echo "-==Arch Is Ready To Be Used==-"
